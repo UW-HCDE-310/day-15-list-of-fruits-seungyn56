4d97e770223fb550c58dc7f8caa551f6abc730c5
Seungyeon Noh <seungyeonnoh@example.com>
2024-11-19 20:42:35 -0800

    finished day 15 assignment

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template
 
+import keys
+
 app = Flask(__name__)
 @app.route("/")
 def index():
@@ -8,4 +10,15 @@ def index():
             {"name": "oranges", "quantity": 2},
             {"name": "strawberries", "quantity": 6}
     ]
-    return render_template("index.html", fruits=fruits)
+
+    new_fruits = []
+
+    for fruit in fruits:
+        if fruit["name"].startswith("o") and fruit["quantity"] < 3:
+            new_fruits.append(fruit)
+
+    return render_template("index.html", fruits=new_fruits)
+
+
+print(keys.MY_SECRET_API_KEY_1)
+print(keys.MY_SECRET_API_KEY_2)
